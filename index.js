0f1139b038109860320a6afe1413fd077073809c
gautam-patil <gpatil1505@gmail.com>
2023-07-05 11:53:29 +0530

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// GET endpoint for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
